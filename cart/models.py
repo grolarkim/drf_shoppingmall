3e877350ff5acc49e8192b7ec70f18a1005df825
grolarkim <audwnswmf@naver.com>
2022-06-27 15:47:21 +0900

    Update __str__ to models

--- cart/models.py
+++ cart/models.py
@@ -10,6 +10,9 @@ class Cart(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="사용자")
     product_option = models.ForeignKey(to=ProductOption, on_delete=models.CASCADE, verbose_name="선택된 상품 옵션")
     count = models.IntegerField(verbose_name="수량", default=1)
+    
+    def __str__(self):
+        return f"{self.user.username}의 장바구니 {self.product_option.name}, 수량: {self.count}"
 
 
 class PaymentMethod(models.Model):
@@ -17,6 +20,9 @@ class PaymentMethod(models.Model):
     updated_at = models.DateTimeField(verbose_name="수정 시각", auto_now=True)
     name = models.CharField(verbose_name="결제 방법", max_length=100, unique=True)
     description = models.TextField(verbose_name="설명", blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"
     
 
 class PurchaseList(models.Model):
@@ -29,3 +35,5 @@ class PurchaseList(models.Model):
     delivery_status = models.CharField(verbose_name="배달 상황", max_length=100)
     payed_money = models.IntegerField(verbose_name="결제 금액")
     
+    def __str__(self):
+        return f"{self.user.username}의 구매목록"
--- review/models.py
+++ review/models.py
@@ -11,6 +11,9 @@ class Review(models.Model):
     user = models.ForeignKey(to=User, verbose_name="사용자", on_delete=models.CASCADE)
     rating = models.IntegerField(verbose_name="평점")
     content = models.TextField(verbose_name="리뷰 내용", blank=True)
+    
+    def __str__(self):
+        return f"{self.product.name}에 대한 {self.user.username}의 리뷰"
 
 
 class Like(models.Model):
@@ -19,3 +22,6 @@ class Like(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="사용자")
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name="상품")
     
+    def __str__(self):
+        return f"{self.product.name}에 대한 {self.user.username}의 좋아요"
+
